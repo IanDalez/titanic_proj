b99aad713ef78d35cdf1104ed2a0266d4c3de1cd
Ian Dalezios <ian.dalezios@outlook.com>
2021-06-21 15:14:20 +0300

    Get titanic's csv files and run code

---
+++ titanic.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 21 15:10:07 2021
+
+@author: Ian Dalezios
+"""
+
+import pandas as pd
+path = "C:/Users/ianda/Documents/titanic_proj/"
+
+df = pd.read_csv(path+"train.csv",',')
+df
